aa07a8b41736ab5e92b3a8578c85d5a893875f75
Mohan A <mohan.dc7batman@gmail.com>
2017-03-13 20:30:09 +0530

    Add project

---
+++ StackExamples/StackExamples/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  StackExamples
+//
+//  Created by AMohan on 13/03/17.
+//  Copyright © 2017 AMohan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
